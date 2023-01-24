8a66812f9c601b0d97116c26417b41d03840bca1
Vinícius Massari <stonefields11@hotmail.com>
2023-01-24 13:05:48 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,59 +1,25 @@
 import "./sass/styles.scss";
 
-const ratingContainer = document.querySelector(".rating-container");
-const thankYouContainer = document.querySelector(".submitted");
-const submitButton = document.querySelector("button");
-const ratingNumber = document.querySelectorAll(".rating-number");
-const spanRating = document.querySelector(".final-rating");
-const spanError = document.querySelector(".error");
+const markAllread = document.querySelector(".mark-read")
+const unreadNumberSpan = document.querySelector(".unread-number")
+const unreadNotification = document.querySelectorAll(".notification.unread")
+const notificationSubject = document.querySelectorAll(".notification-subject.unread")
+const events = ["click", "touchstart"]
 
-const events = ["click", "touchlist"];
-let rating = 0;
 
-function toggleRating(event) {
-  this.classList.remove("hover");
-  event.preventDefault();
-  rating = this.innerText;
-  ratingNumber.forEach((rating) => {
-    rating.classList.remove("active");
-  });
-  this.classList.add("active");
+
+function markAsRead(event) {
+    event.preventDefault()
+    unreadNotification.forEach(unread =>{
+        unread.classList.remove("unread")
+    })
+    notificationSubject.forEach(notification =>{
+        notification.classList.remove("unread")
+    })
+    unreadNumberSpan.innerText = "0"
 }
 
-function submitRating(event) {
-  event.preventDefault();
-  if (rating === 0) {
-    spanError.classList.add("active");
-  } else {
-    spanError.classList.remove("active");
-    ratingContainer.style.display = "none";
-    spanRating.innerText = `You selected ${rating} out of 5`;
-    thankYouContainer.classList.add("active");
-  }
-}
+events.forEach(userEvent =>{
+    markAllread.addEventListener(userEvent, markAsRead)
+})
 
-function handleMouse() {
-  this.classList.add("hover");
-}
-
-function removeMouseEvent() {
-  this.classList.remove("hover");
-}
-
-ratingNumber.forEach((number) => {
-  number.addEventListener("mouseover", handleMouse);
-});
-
-ratingNumber.forEach((number) => {
-  number.addEventListener("mouseout", removeMouseEvent);
-});
-
-events.forEach((userEvent) => {
-  ratingNumber.forEach((rating) => {
-    rating.addEventListener(userEvent, toggleRating);
-  });
-});
-
-events.forEach((userEvent) => {
-  submitButton.addEventListener(userEvent, submitRating);
-});
